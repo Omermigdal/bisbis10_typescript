eb0d045164e5d9ac8c7d6621b872fc6b38106894
Omer Migdal <103674473+Omermigdal@users.noreply.github.com>
2024-05-25 20:13:48 +0300

    Update orderController.ts

--- src/controllers/orderController.ts
+++ src/controllers/orderController.ts
@@ -7,8 +7,9 @@ export const makeOrder = async (req: Request, res: Response) => {
     const { restaurantId, orderItems } = req.body;
     
     if (!restaurantId) {
-        return res.status(404).json({ error: 'Restaurant not found' });
+        return res.status(400).json({ error: 'Restaurant not found' });
     }
+    const restaurant = await prisma.restaurant.findUnique({where: {id:restaurantId}})
     
     const dishes = await prisma.dish.findMany({
         where: {
@@ -17,28 +18,14 @@ export const makeOrder = async (req: Request, res: Response) => {
             },
         },
     });
-    /// check if this is ok
+   
     
-    const orderItemsData = orderItems.map((item: { dishId: any, quantity: number }) => {
-        const dish = dishes.find((d) => d.id === item.dishId);
-        if (!dish) {
-            throw new Error(`Dish with id ${item.dishId} not found`);
-        }
-        return {
-            dishId: item.dishId,
-            quantity: item.quantity,
-            price: Number(dish.price) * item.quantity, // calculate price for each order item
-        };
-    });
-    
-    const totalAmount = orderItemsData.reduce((acc: number, item: { price: number }) => acc + item.price, 0);
     
     const order = await prisma.order.create({
         data: {
             restaurantId,
-            totalAmount,
             orderItems: {
-                create: orderItemsData,
+                createMany: {data: orderItems,},
             },
         },
     });
