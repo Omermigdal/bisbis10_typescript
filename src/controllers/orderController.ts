1fa983c6a6166033381de0d5d1d6c7fcbce56bf5
Omer Migdal <omermigdali@gmail.com>
2024-05-25 21:33:27 +0300

    added order service

--- src/controllers/orderController.ts
+++ src/controllers/orderController.ts
@@ -1,34 +1,14 @@
 import { Request, Response } from 'express';
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import { Order_create } from '../services/orderService';
 
 export const makeOrder = async (req: Request, res: Response) => {
+    try{
     const { restaurantId, orderItems } = req.body;
-    
-    if (!restaurantId) {
-        return res.status(400).json({ error: 'Restaurant not found' });
+    const newOrder= await Order_create(restaurantId,orderItems);
+    res.status(200).json(newOrder.id); 
     }
-    const restaurant = await prisma.restaurant.findUnique({where: {id:restaurantId}})
-    
-    const dishes = await prisma.dish.findMany({
-        where: {
-            id: {
-                in: orderItems.map((item: { dishId: any }) => item.dishId),
-            },
-        },
-    });
-   
-    
-    
-    const order = await prisma.order.create({
-        data: {
-            restaurantId,
-            orderItems: {
-                createMany: {data: orderItems,},
-            },
-        },
-    });
-
-    res.status(200).json(order.id); //TODO : check if i cn serialize
+    catch(errorMessage)
+    {
+      return errorMessage;
+    }
 }
---
+++ src/services/orderService.ts
@@ -0,0 +1,24 @@
+import { Order, PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+
+
+export const Order_create = async (restaurantId:number,orderItems:any) =>
+    {
+      try{
+        const order = await prisma.order.create({
+            data: {
+                restaurantId,
+                orderItems: {
+                    createMany: {data: orderItems,},
+                },
+            },
+        });
+        return order;
+
+    }
+    catch(err:any){
+        throw err.message
+    }
+};
